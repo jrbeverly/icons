82727da2dec8675fa1db5945743cb05bb4cd511f
jrbeverly <jrbeverly@live.com>
2017-01-28 22:32:44 -0500

    Setting up build scale (& profile icon)

--- .build/build.sh
+++ .build/build.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 function_name () { 
-    rsvg-convert -h 32 icon.svg > icon-32.png
-    rsvg-convert -h 64 icon.svg > icon-64.png
-    rsvg-convert -h 128 icon.svg > icon-128.png
+    rsvg-convert -h 480 icon.svg > icon-480.png
 }
 
 cd projects/
 for dir in *
 do
   test -d "$dir" || continue
-  ( cd $dir/ && function_name )
+  ( cd "$dir/" && function_name )
 done
--- .build/clean.sh
+++ .build/clean.sh
@@ -4,5 +4,5 @@ cd projects/
 for dir in *
 do
   test -d "$dir" || continue
-  ( cd $dir/ && rm -rf *.png )
+  ( cd "$dir/" && rm -rf *.png )
 done
